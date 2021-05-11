a2872dcc515eb314d9dbc7593add3cf4d7cb4b79
kuribayashi <tktkchestnut@gmail.com>
2021-05-11 13:41:31 +0900

    5th commit

--- flask/flask_bolt_run.py
+++ flask/flask_bolt_run.py
@@ -18,16 +18,43 @@ def log_request(logger, body, next):
     logger.debug(body)
     return next()
 
+@app.message("hello")
+def message_hello(message,say):
+    say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "会議後アンケートの入力をお願いします！"
+                },
+                "accessory":{
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "回答を送信する"
+                    },
+                    "action_id": "button_click"
+                }
+            }
+        ]
+    )
 
 @app.event("app_mention")
 def event_test(body, say, logger):
-    logger.info(body)
+    #logger.info(body)
     say("What's up?")
 
 
 @app.event("message")
 def handle_message():
     pass
+
+@app.action("meeting_finished")
+def send_interview(body,ack,say):
+    ack()
+    say("What's up?")
+    
 
 
 flask_app = Flask(__name__)
@@ -38,6 +65,6 @@ def slack_events():
     return handler.handle(request)
 
 
-# FLASK_APP=app.py FLASK_ENV=development flask run -p 3000
+# FLASK_APP=app.py FLASK_ENV=development flask run -p 5000
 if __name__ == "__main__":
     flask_app.run(debug=True, port=5000)
